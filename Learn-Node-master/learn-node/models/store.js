d06a87390b3205bc5415ee9ce0c4a5e7cf7589c6
marko delgadillo <markoneseven@gmail.com>
2018-05-16 20:16:21 -0700

    #1 Code from the first three modules of the course along with the packages installed.

--- Learn-Node-master/learn-node/models/store.js
+++ Learn-Node-master/learn-node/models/store.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
+// assigns Promise to mongoose to the ES6 Promise
 mongoose.Promise = global.Promise
+// library slugs makes URL friendly names for our slugs
 const slug = require('slugs')
 
 const storeSchema = new mongoose.Schema({
@@ -14,6 +16,19 @@ const storeSchema = new mongoose.Schema({
     trim: true
   },
   tags: [String]
+})
+
+// this will happen BEFORE a Schema is saved to the Database
+storeSchema.pre('save', function(next) {
+  if (!this.isModified('name')) {
+    // if the name is NOT modified
+    next() // skip it
+    return // stop this function from running
+  }
+  // if the name IS changed, it will run this
+  this.slug = slug(this.name)
+  next()
+  // TODO make more resiliant so slugs are unique
 })
 
 module.exports = mongoose.model('Store', storeSchema)
